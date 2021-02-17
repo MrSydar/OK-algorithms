c35ad7be92daeb7b76f28ccbf2e75c4f66066db3
MrSydar <dengrom1@gmail.com>
2021-02-17 15:51:38 +0100

    Gorno-dolne full :apple:

--- gorno_dolne.py
+++ gorno_dolne.py
@@ -48,12 +48,8 @@ def applyPath(pIndex: int, maxFlow: int, path: list, graph: list, graphUsed: lis
             graphUsed[path[pIndex + 1]][path[pIndex]] -= maxFlow
     return maxFlow
 
-def dinic(fromNode: int, toNode: int, graph: list, nodePriority: list, nodeNames: list):
+def dinic(fromNode: int, toNode: int, graph: list, graphUsed: list, nodePriority: list, nodeNames: list):
     n = len(graph)
-    
-    graphUsed = []
-    for i in range(n):
-        graphUsed.append([0] * n)
     
     deltaSum = 0
     while True:
@@ -73,6 +69,13 @@ def dinic(fromNode: int, toNode: int, graph: list, nodePriority: list, nodeNames
 
             deltaSum += delta
         else:
+            print('\nNasycone krawedzi: \033[92m')
+            for i in range(n):
+                for x in range(n):
+                    if graph[i][x] != 0 and (graph[i][x] - graphUsed[i][x]) == 0 and i != x:
+                        print('{} -> {}'.format(nodeNames[i], nodeNames[x]))
+            print('\033[0m')
+
             return deltaSum
 
 def upperlower(fromNode: int, toNode: int, graphLowerBound: list, graphUpperBound: list, nodePriority: list, nodeNames: list):
@@ -99,7 +102,7 @@ def upperlower(fromNode: int, toNode: int, graphLowerBound: list, graphUpperBoun
     nodePriority.insert(0, 0)
     nodePriority.append(min(nodePriority) - 1)
 
-    print('NEW GRAPH:\033[91m')
+    print('GORNO DOLNY GRAF:\033[91m')
     for i in range(n):
         print('{}\t'.format(nodeNames[i]), end='')
     print('\033[0m')
@@ -108,28 +111,102 @@ def upperlower(fromNode: int, toNode: int, graphLowerBound: list, graphUpperBoun
         for x in range(n):
             print('{}\t'.format(graph[i][x]), end='')
         print('\033[91m{}\033[0m'.format(nodeNames[i]))
+    print()
 
-    return dinic(0, n - 1, graph, nodePriority, nodeNames)
+    graphUsed = []
+    for i in range(n):
+        graphUsed.append([0] * n)
+
+    d1 = dinic(0, n - 1, graph, graphUsed, nodePriority, nodeNames)
+    print('Delta z sieci gorno-dolnej: {}'.format(d1))
+
+    if sum([graph[0][i] - graphUsed[0][i] for i in range(n)]) + sum([graph[i][-1] - graphUsed[i][-1] for i in range(n)]) != 0:
+        print('\033[91mNie istnieje poprawnego dopuszczalnego rozwiazania w tej sieci, poniewaz nie wszystkie luki nasycone\033[0m')
+        return d1
+    
+    n -= 2
+    newGraph = []
+    for i in range(n):
+        newGraph.append([0] * (n))
+        for x in range(n):
+            newGraph[i][x] = graphLowerBound[i][x] + graphUsed[i + 1][x + 1]
+    newGraph[-1][0] = 0
+
+    print('\nWartosci \033[92mmin + przeplyw\033[0m w grafie po sieci gorno-dolnej: \033[91m')
+    for i in range(n):
+        print('{}\t'.format(nodeNames[i + 1]), end='')
+    print('\033[0m\033[92m')
+
+    for i in range(n):
+        for x in range(n):
+            print('{}\t'.format(newGraph[i][x]), end='')
+        print('\033[0m\033[91m{}\033[0m\033[92m'.format(nodeNames[i + 1]))
+    print('\033[0m')
+    
+    for i in range(n):
+        for x in range(n):
+            newGraph[i][x] = graphUpperBound[i][x] - newGraph[i][x]
+
+    print('Nowy \033[92mmax - (min + przeplyw)\033[0m graf po sieci gorno-dolnej: \033[91m')
+    for i in range(n):
+        print('{}\t'.format(nodeNames[i + 1]), end='')
+    print('\033[0m\033[92m')
+
+    for i in range(n):
+        for x in range(n):
+            print('{}\t'.format(newGraph[i][x]), end='')
+        print('\033[0m\033[91m{}\033[0m\033[92m'.format(nodeNames[i + 1]))
+    print('\033[0m')
+    
+    graphUsed = []
+    for i in range(n):
+        graphUsed.append([0] * n)
+ 
+    d2 = dinic(0, n - 1, newGraph, graphUsed, nodePriority[1:-1], nodeNames[1:-1])
+    print('Delta z drugiej sieci: {}\n'.format(d2))
+
+    return d1 + d2
 
 
 
-myNodeNames = ['s', 'A', 'B', 't']
-#             s  A  B  t
-myPriority = [0, 1, 2, -1]
+myNodeNames = ['s', '2', '3', '4', 't']
+#             s  2  3  4  t
+myPriority = [0, 1, 4, 2, -1]
 myGraphLowerBound = [
-# fr:s  A  B  t     to
-    [0, 0, 1, 0], # s
-    [0, 0, 0, 2], # A
-    [0, 0, 0, 2], # B
-    [0, 0, 0, 0]  # t
+# fr:s  2  3  4  t     to
+    [0, 2, 1, 0, 0], # s
+    [0, 0, 0, 1, 1], # 2
+    [0, 2, 0, 1, 0], # 3
+    [0, 0, 0, 0, 2], # 4
+    [0, 0, 0, 0, 0]  # t
 ]
 myGraphUpperBound = [
-# fr:s  A  B  t     to
-    [0, 1, 2, 0], # s
-    [0, 0, 0, 3], # A
-    [0, 0, 0, 3], # B
-    [0, 0, 0, 0]  # t
+# fr:s  2  3  4  t     to
+    [0, 3, 5, 0, 0], # s
+    [0, 0, 0, 2, 7], # 2
+    [0, 5, 0, 3, 0], # 3
+    [0, 0, 0, 0, 5], # 4
+    [0, 0, 0, 0, 0]  # t
 ]
+myDeltaSum = upperlower(0, 4, myGraphLowerBound, myGraphUpperBound, myPriority, myNodeNames)
 
-myDeltaSum = upperlower(0, 3, myGraphLowerBound, myGraphUpperBound, myPriority, myNodeNames)
+# myNodeNames = ['s', 'A', 'B', 't']
+# #             s  A  B  t
+# myPriority = [0, 1, 2, -1]
+# myGraphLowerBound = [
+# #TO :s  A  B  t     from
+#     [0, 0, 1, 0], # s
+#     [0, 0, 0, 2], # A
+#     [0, 0, 0, 2], # B
+#     [0, 0, 0, 0]  # t
+# ]
+# myGraphUpperBound = [
+# #TO :s  A  B  t     from
+#     [0, 1, 2, 0], # s
+#     [0, 0, 0, 3], # A
+#     [0, 0, 0, 3], # B
+#     [0, 0, 0, 0]  # t
+# ]
+# myDeltaSum = upperlower(0, 4, myGraphLowerBound, myGraphUpperBound, myPriority, myNodeNames)
+
 print('Max flow is {}'.format(myDeltaSum))
