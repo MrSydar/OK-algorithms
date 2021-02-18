f233418de3a09d42dffe827b6f8d1535b4cea449
MrSydar <dengrom1@gmail.com>
2021-02-18 08:59:39 +0100

    Critical bug solved :bug:

--- plecak_I.py
+++ plecak_I.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def plecak2(b: int, w: list, p: list):
     table = [[0 for x in range(b + 1)] for x1 in range(len(w) + 1)]
     
@@ -8,8 +10,16 @@ def plecak2(b: int, w: list, p: list):
     sack = []
     backTrack(len(table) - 1, len(table[0]) - 1, table, w, sack)
 
-    printTable(table)
-    print(sack)
+    sack.reverse()
+    tmpSack = deepcopy(sack)
+    printTable(table, tmpSack)
+
+    print('ELEMENTS: \u001b[32m', end='')
+    for i in range(len(sack)):
+        print('{} '.format(sack[i][0]), end='')
+    print('\u001b[0m')
+
+    print('KNAPSACK VALUE: \u001b[32m{}\u001b[0m'.format(sum([p[x[0] - 1] for x in sack])))
 
 def backTrack(i: int, j: int, table: list, w: list, sack: list):
     if i - 1 < 0:
@@ -17,11 +27,10 @@ def backTrack(i: int, j: int, table: list, w: list, sack: list):
     elif table[i][j] == table[i-1][j]:
         backTrack(i - 1, j, table, w, sack)
     else:
-        sack.append(i)
-        table[i][j] *= -1
+        sack.append([i,j])
         backTrack(i - 1, j - w[i - 1], table, w, sack)
 
-def printTable(table: list):
+def printTable(table: list, sack: list):
     print('\033[94m'+'i\\j\t', end='')
     for j in range(len(table[0])):
         print('{}\t'.format(j), end='')
@@ -29,10 +38,14 @@ def printTable(table: list):
     for i in range(len(table)):
         print('\033[94m'+'{}\t'.format(i)+'\033[0m', end='')
         for j in range(len(table[0])):
-            print('\033[91m'+'{} \t'.format(table[i][j]*-1)+'\033[0m' if table[i][j] < -1 else '{} \t'.format(table[i][j]), end='')
+            if len(sack) > 0 and sack[0][0] == i and sack[0][1] == j:
+                print('\033[91m'+'{} \t'.format(table[i][j])+'\033[0m', end='')
+                sack.pop(0)
+            else:
+                print('{} \t'.format(table[i][j]), end='')
         print()
 
-B = 8           #rozmiar plecaka
+B = 8          #rozmiar plecaka
 W = [2,1,4,4]  #rozmiary
 P = [4,3,6,8]  #wartosci
 
--- plecak_II.py
+++ plecak_II.py
@@ -1,85 +1,89 @@
+from copy import deepcopy
+
 #-1 is infinity
 
-class plecak:
-    def infSum(self, a: int, b: int):
-        if a < 0 or b < 0:
-            return -1
-        else:
-            return a + b
+def infSum(a: int, b: int):
+    if a < 0 or b < 0:
+        return -1
+    else:
+        return a + b
 
-    def infMin(self, a: int, b: int):
-        if a < 0 and b < 0:
-            return -1
-        elif a < 0:
-            return b
-        elif b < 0:
-            return a
-        elif a > b:
-            return b
-        else:
-            return a
+def infMin(a: int, b: int):
+    if a < 0 and b < 0:
+        return -1
+    elif a < 0:
+        return b
+    elif b < 0:
+        return a
+    elif a > b:
+        return b
+    else:
+        return a
 
-    def getVal(self, table: list, i: int, j: int, w: list, s: list):
-        if w[i - 1] > j:
-            table[i][j] = table[i - 1][j]
-        else:
-            table[i][j] = self.infMin(
-                table[i - 1][j], 
-                self.infSum(
-                    table[i - 1][j - w[i - 1]], 
-                    s[i - 1]
-                    )
+def getVal(table: list, i: int, j: int, w: list, s: list):
+    if w[i - 1] > j:
+        table[i][j] = table[i - 1][j]
+    else:
+        table[i][j] = infMin(
+            table[i - 1][j], 
+            infSum(
+                table[i - 1][j - w[i - 1]], 
+                s[i - 1]
                 )
+            )
 
-    def plecak2(self, b: int, w: list, s: list):
-        table = [[0] + [-1] * (sum(w))]
-        
-        for i in range(1, len(w) + 1):
-            table += [[0] * (sum(w) + 1)]
-            for j in range(1, sum(w) + 1):
-                self.getVal(table, i, j, w, s)
+def plecak2(b: int, w: list, s: list):
+    table = [[0] + [-1] * (sum(w))]
+    
+    for i in range(1, len(w) + 1):
+        table += [[0] * (sum(w) + 1)]
+        for j in range(1, sum(w) + 1):
+            getVal(table, i, j, w, s)
 
-        j = len(table[0]) - 1
-        while table[-1][j] > b:
-            j -= 1
-        sack = []
-        self.backTrack(len(table) - 1, j, table, w, sack)
+    j = len(table[0]) - 1
+    while table[-1][j] > b:
+        j -= 1
+    sack = []
+    backTrack(len(table) - 1, j, table, w, sack)
+    sack.reverse()
 
-        self.printTable(table)
-        print(sack)
+    tmpSack = deepcopy(sack)
+    printTable(table, tmpSack)
 
-    def backTrack(self, i: int, j: int, table: list, w: list, sack: list):
-        if i - 1 < 0:
-            return
-        elif table[i][j] == table[i-1][j]:
-            self.backTrack(i - 1, j, table, w, sack)
-        else:
-            sack.append(i)
-            table[i][j] *= -1
-            self.backTrack(i - 1, j - w[i - 1], table, w, sack)
+    print('ELEMENTS: \u001b[32m', end='')
+    for i in range(len(sack)):
+        print('{} '.format(sack[i][0]), end='')
+    print('\u001b[0m')
 
-    def printTable(self, table: list):
-        print('\033[94m'+'i\\j\t', end='')
+    print('KNAPSACK VALUE: \u001b[32m{}\u001b[0m'.format(sum([w[x[0] - 1] for x in sack])))
+    
+
+def backTrack(i: int, j: int, table: list, w: list, sack: list):
+    if i - 1 < 0:
+        return
+    elif table[i][j] == table[i-1][j]:
+        backTrack(i - 1, j, table, w, sack)
+    else:
+        sack.append([i,j])
+        backTrack(i - 1, j - w[i - 1], table, w, sack)
+
+def printTable(table: list, sack: list):
+    print('\033[94m'+'i\\j\t', end='')
+    for j in range(len(table[0])):
+        print('{}\t'.format(j), end='')
+    print('\033[0m')
+    for i in range(len(table)):
+        print('\033[94m'+'{}\t'.format(i)+'\033[0m', end='')
         for j in range(len(table[0])):
-            print('{}\t'.format(j), end='')
-        print('\033[0m')
-        for i in range(len(table)):
-            print('\033[94m'+'{}\t'.format(i)+'\033[0m', end='')
-            for j in range(len(table[0])):
-                print('∞ \t' if table[i][j] == -1 else '\033[91m'+'{} \t'.format(table[i][j]*-1)+'\033[0m' if table[i][j] < -1 else '{} \t'.format(table[i][j]), end='')
-            print()
-
-    def __init__(self, B, W, S):
-        self.B = B
-        self.W = W
-        self.S = S
+            if len(sack) != 0 and sack[0][0] == i and sack[0][1] == j:
+                print('\033[91m'+'{} \t'.format(table[i][j])+'\033[0m', end='')
+                sack.pop(0)
+            else:
+                print('∞ \t' if table[i][j] == -1 else '{} \t'.format(table[i][j]), end='')
+        print()
     
-    def solve(self):
-        self.plecak2(B,W,S)
-    
-B = 8           #rozmiar plecaka
+B = 8            #rozmiar plecaka
 W = [3,4,2,6,1]  #wartosci
 S = [5,3,2,4,3]  #rozmiary
 
-p = plecak(B, W, S)
-p.solve()
+plecak2(B, W, S)
